83c991ab7d200446021d01169b02a6a212d30369
Marc Lou <marc.louvion@gmail.com>
2023-10-30 16:09:28 +0800

    (new) Support for Next 13

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
+import { Viewport } from "next";
 import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
 
 const font = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+  themeColor: config.colors.main,
+  width: "device-width",
+  maximumScale: 1,
+  initialScale: 1,
+};
 
 // This adds default SEO tags to all pages in our app.
 // You can override them in each page passing params to getSOTags() function.
