43cb9cf284f0bfe06b035e7cc0a37d44560f2b21
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-12-15 23:56:01 -0300

    refactor: remove unecessary verification and rename ButtonSignin to ButtonSignIn

--- components/ButtonSignin.tsx
+++ components/ButtonSignin.tsx
@@ -10,7 +10,7 @@ import config from "@/config";
 // A simple button to sign in with our providers (Google & Magic Links).
 // It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
 // If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
-const ButtonSignin = ({
+const ButtonSignIn = ({
   text = "Entrar",
   extraStyle,
 }: {
@@ -66,4 +66,4 @@ const ButtonSignin = ({
   );
 };
 
-export default ButtonSignin;
+export default ButtonSignIn;
