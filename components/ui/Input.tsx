b4db600b06d96e0a2798f7f9824b619f915dfd4a
jeffsLM <jeffs.dev@gmail.com>
2024-02-12 21:11:40 -0300

    feat: new ui components

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -4,15 +4,25 @@ import { cn } from "@/libs/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  errorName?: string
+  errorName?: string;
+  name: string;
+  label?: string;
+  classNameLabel?: string;
+  classNameContainer?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ errorName, className, type, ...props }, ref) => {
+  ({ errorName, className, type, name, label, classNameLabel, classNameContainer, ...props }, ref) => {
     return (
-      <div>
+      <div className={classNameContainer}>
+        {label &&
+          <label htmlFor={name} className={classNameLabel}>
+            {label}
+          </label>
+        }
         <input
           type={type}
+          id={name}
           className={cn(
             `flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 ${errorName ? 'input-error' : ''}`,
             className
