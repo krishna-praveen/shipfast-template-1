80e9901eaf0f97f795defc212f37d44027c6335c
jeffsLM <jeffs.dev@gmail.com>
2024-02-13 16:23:43 -0300

    feat: global constants

---
+++ constants/daysWorkouts/index.ts
@@ -0,0 +1,41 @@
+export const DAYS_WORKOUT = [
+  {
+    value: 'monday',
+    label: 'Segunda',
+  },
+  {
+    value: 'tuesday',
+    label: 'Terça-feira',
+  },
+  {
+    value: 'wednesday',
+    label: 'Quarta-feira',
+  },
+  {
+    value: 'thursday',
+    label: 'Quinta-feira',
+  },
+  {
+    value: 'friday',
+    label: 'Sexta-feira',
+  },
+  {
+    value: 'saturday',
+    label: 'Sábado',
+  },
+  {
+    value: 'sunday',
+    label: 'Domingo',
+  },
+];
+
+export const DAYS_WORKOUT_RANGE = [
+  {
+    value: 'alldays',
+    label: 'Todos os dias',
+  },
+  {
+    value: 'onlybusydays',
+    label: 'Apenas Dias Úteis',
+  },
+]
