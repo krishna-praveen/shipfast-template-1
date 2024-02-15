b0ef448f29594dd6bc9e28aa346e2600a91801bc
jeffsLM <jeffs.dev@gmail.com>
2024-02-15 19:29:43 -0300

    feat: wip

--- app/(private)/workouts/register/page.tsx
+++ app/(private)/workouts/register/page.tsx
@@ -1,9 +1,24 @@
 'use client';
 // import { useState } from "react";
 
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { z } from 'zod';
+
 import Layout from "@/components/layout/Layout";
-import { Button } from '@/components/ui/Button'
+import { Button } from '@/components/ui/Button';
+import { CalendarInput } from '@/components/ui/Form/CalendarInput';
+import { ComboBoxInput } from '@/components/ui/Form/ComboBoxInput';
+import { TextInput } from '@/components/ui/Form/TextInput';
+import { Input } from '@/components/ui/Input';
 import { TopBar } from "@/components/ui/TopBar";
+import { DAYS_WORKOUT, DAYS_WORKOUT_RANGE } from '@/constants';
+import { useSchema } from '@/hooks/useSchema';
+import { useListStudents } from '@/services/hooks/useListStudents';
+
+type NewWorkoutProps = Required<z.infer<typeof useSchema.newWorkout>>;
 
 export interface ExerciseInterface {
   name: string;
@@ -14,6 +29,11 @@ export interface ExerciseInterface {
 }
 
 export default function Register() {
+
+  const { data: listStudents } = useListStudents({ refetchOnWindowFocus: false });
+  const studentsData = listStudents?.map(student => {
+    return { label: student.name + ' ' + student.surname, value: student.id }
+  }) || [];
   // const [workoutTabs, setWorkoutTabs] = useState<string>("ABC");
   // const [selectedWorkoutTab, setSelectedWorkoutTab] = useState<string>("A");
   // const [exercises, setExercises] = useState<{ [key: string]: ExerciseInterface[] }>({});
@@ -48,6 +68,15 @@ export default function Register() {
   //   setEditingExercise({ exercise: null, tab: '', index: -1 });
   // };
 
+
+  const methods = useForm<NewWorkoutProps>({
+    resolver: zodResolver(useSchema.newWorkout),
+  });
+
+  const onSubmit = (data: NewWorkoutProps) => {
+    console.log(data);
+  }
+
   return (
     <Layout>
 
@@ -61,6 +90,63 @@ export default function Register() {
         </TopBar.Action>
       </TopBar.Root>
 
+      <form onSubmit={methods.handleSubmit(onSubmit)} className="mt-10 grid grid-cols-2 gap-4 lg:grid-cols-6">
+        <FormProvider {...methods}>
+          <TextInput
+            label='Descrição do treino'
+            placeholder='Exemplo: Semana 1'
+            classNameContainer='col-span-2'
+            name='descriptionWorkout'
+          />
+
+          <CalendarInput
+            name='startDate'
+            label='Início'
+          />
+
+          <CalendarInput
+            name='endDate'
+            label='Término'
+          />
+
+          <CalendarInput
+            name='revaluation'
+            label='Reavaliação'
+          />
+
+          <ComboBoxInput
+            name='daysOfWorkout'
+            label='Dias de treino'
+            data={DAYS_WORKOUT}
+            subData={DAYS_WORKOUT_RANGE}
+            useSubData
+            placeholder='Selecione uma opção'
+          />
+
+          <ComboBoxInput
+            name='student'
+            label='Aluno'
+            data={studentsData}
+            classNameContainer='col-span-2'
+            placeholder='Selecione uma opção'
+          />
+
+
+          <TextInput
+            label='Objetivo'
+            classNameContainer='col-span-2'
+            placeholder='Exemplo: Hipertrofia'
+            name='objective' />
+
+          <TextInput
+            label='Observação'
+            classNameContainer='col-span-2'
+            placeholder='Exemplo: Mobilidade todos os dias antes do treino'
+            name='obs' />
+        </FormProvider>
+        <button type="submit">Cadastrar</button>
+
+      </form>
 
     </Layout>
   )
