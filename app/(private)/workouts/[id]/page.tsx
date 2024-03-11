6e02b39f9fa22e23ba1a1627e1e75087712615bd
jeffsLM <jeffes.dev@gmail.com>
2024-03-10 21:41:28 -0300

    fix: wip

--- app/(private)/workouts/[id]/page.tsx
+++ app/(private)/workouts/[id]/page.tsx
@@ -1,106 +1,109 @@
-'use client';
+// 'use client';
 
-import { useState } from "react";
+// import { useState } from "react";
 
-import Layout from "@/components/layout/Layout";
-import { AddExercise } from "@/components/workouts/AddExercise";
-import { CardExercise } from "@/components/workouts/CardExercise";
-import { EditExercise } from "@/components/workouts/EditExercise";
-import { TopBar } from "@/components/workouts/TopBar";
+// import Layout from "@/components/layout/Layout";
 
-import { useListWorkoutsById } from '@/services/hooks/useListWorkoutsById';
+// import { useListWorkoutsById } from '@/services/hooks/useListWorkoutsById';
 
-import { ExerciseInterface, WorkoutInterface } from '@/types/models/listWorkoutsById.model';
+// import { ExerciseInterface, WorkoutInterface } from '@/types/models/listWorkoutsById.model';
 
-export default function Workout({ params }: { params: { id: string } }) {
-  const [workoutTabs, setWorkoutTabs] = useState("ABC");
-  const [selectedWorkoutTab, setSelectedWorkoutTab] = useState("A");
-  const [exercises, setExercises] = useState<{ [key: string]: ExerciseInterface[] }>({});
-  const [workout, setWorkout] = useState<WorkoutInterface>();
-  const [editingExercise, setEditingExercise] = useState({ exercise: null, tab: '', index: -1 });
+// import { AddExercise } from "@/components/workouts/AddExercise";
+// import { CardExercise } from "@/components/workouts/CardExercise";
+// import { EditExercise } from "@/components/workouts/EditExercise";
+// import { TopBar } from "@/components/workouts/TopBar";
 
-  const { id: workoutId } = params
 
-  const addExercise = (exercise: ExerciseInterface) => {
-    setExercises(prevExercises => ({
-      ...prevExercises,
-      [selectedWorkoutTab]: [...(prevExercises[selectedWorkoutTab] || []), exercise]
-    }));
-  };
 
-  const deleteExercise = (tab: string, index: number) => {
-    setExercises(prevExercises => ({
-      ...prevExercises,
-      [tab]: prevExercises[tab].filter((_, i) => i !== index),
-    }));
-  };
+// export default function Workout({ params }: { params: { id: string } }) {
+//   const [workoutTabs, setWorkoutTabs] = useState("ABC");
+//   const [selectedWorkoutTab, setSelectedWorkoutTab] = useState("A");
+//   const [exercises, setExercises] = useState<{ [key: string]: ExerciseInterface[] }>({});
+//   const [workout, setWorkout] = useState<WorkoutInterface>();
+//   const [editingExercise, setEditingExercise] = useState({ exercise: null, tab: '', index: -1 });
 
-  const editExercise = (exercise: ExerciseInterface, tab: string, index: number) => {
-    setEditingExercise({ exercise, tab, index });
-  };
+//   const { id: workoutId } = params
 
-  const saveEditedExercise = (type: string, editedExercise: ExerciseInterface) => {
-    setExercises(prevExercises => ({
-      ...prevExercises,
-      [type]: prevExercises[type].map((ex, i) =>
-        i === editingExercise.index ? editedExercise : ex
-      ),
-    }));
+//   const addExercise = (exercise: ExerciseInterface) => {
+//     setExercises(prevExercises => ({
+//       ...prevExercises,
+//       [selectedWorkoutTab]: [...(prevExercises[selectedWorkoutTab] || []), exercise]
+//     }));
+//   };
 
-    setEditingExercise({ exercise: null, tab: '', index: -1 });
-  };
+//   const deleteExercise = (tab: string, index: number) => {
+//     setExercises(prevExercises => ({
+//       ...prevExercises,
+//       [tab]: prevExercises[tab].filter((_, i) => i !== index),
+//     }));
+//   };
 
-  useListWorkoutsById({
-    workoutId, options: {
-      enabled: !!workoutId,
-      onSuccess: (data) => {
-        const { exercises, ...rest } = data
-        setExercises(exercises || {});
-        setWorkout({ ...rest, exercises });
-      }
-    }
-  })
+//   const editExercise = (exercise: ExerciseInterface, tab: string, index: number) => {
+//     setEditingExercise({ exercise, tab, index });
+//   };
 
-  return (
-    <Layout>
+//   const saveEditedExercise = (type: string, editedExercise: ExerciseInterface) => {
+//     setExercises(prevExercises => ({
+//       ...prevExercises,
+//       [type]: prevExercises[type].map((ex, i) =>
+//         i === editingExercise.index ? editedExercise : ex
+//       ),
+//     }));
 
-      <TopBar title="Treino" workout={workout} onChangeTabsType={(typeTabs: string) => setWorkoutTabs(typeTabs)} tabsType={workoutTabs} exercises={exercises} />
+//     setEditingExercise({ exercise: null, tab: '', index: -1 });
+//   };
 
-      <div className="bg-base-100 w-full">
-        <div role="tablist" aria-label="Workouts" className="tabs block border-primary px-10">
-          {
-            workoutTabs.split('').map((workoutTab, index) => (
-              <input key={index} type="radio" name="workout_tabs" onChange={() => setSelectedWorkoutTab(workoutTab)} role="tab" className="tab h-auto px-8 py-5 font-semibold ![border-color:var(--secondary)] checked:border-b-2 checked:text-secondary" aria-label={`Treino ${workoutTab}`} defaultChecked={index === 0} />
-            ))
-          }
-        </div>
+//   useListWorkoutsById({
+//     workoutId, options: {
+//       enabled: !!workoutId,
+//       onSuccess: (data) => {
+//         const { exercises, ...rest } = data
+//         setExercises(exercises || {});
+//         setWorkout({ ...rest, exercises });
+//       }
+//     }
+//   })
 
-        <div className="bg-image flex flex-col gap-8 p-10">
+//   return (
+//     <Layout>
 
-          <div className="flex items-center gap-5">
-            <h2 className="text-2xl font-bold">Exercícios</h2>
-            <label htmlFor="addExercise" className="btn btn-outline btn-primary">Adicionar Exercício</label>
-          </div>
+//       <TopBar title="Treino" workout={workout} onChangeTabsType={(typeTabs: string) => setWorkoutTabs(typeTabs)} tabsType={workoutTabs} exercises={exercises} />
 
-          <div className="flex flex-wrap justify-between">
-            {
-              (exercises[selectedWorkoutTab] || []).map((exercise, index) => (
-                <CardExercise
-                  key={index}
-                  {...exercise}
-                  onEditExercise={() => editExercise(exercise, selectedWorkoutTab, index)}
-                  onDeleteExercise={() => deleteExercise(selectedWorkoutTab, index)}
-                />
-              ))
-            }
-          </div>
-        </div>
+//       <div className="bg-base-100 w-full">
+//         <div role="tablist" aria-label="Workouts" className="tabs block border-primary px-10">
+//           {
+//             workoutTabs.split('').map((workoutTab, index) => (
+//               <input key={index} type="radio" name="workout_tabs" onChange={() => setSelectedWorkoutTab(workoutTab)} role="tab" className="tab h-auto px-8 py-5 font-semibold ![border-color:var(--secondary)] checked:border-b-2 checked:text-secondary" aria-label={`Treino ${workoutTab}`} defaultChecked={index === 0} />
+//             ))
+//           }
+//         </div>
 
-      </div>
+//         <div className="bg-image flex flex-col gap-8 p-10">
 
-      <AddExercise onAddExercise={addExercise} selectedWorkoutTab={selectedWorkoutTab} />
+//           <div className="flex items-center gap-5">
+//             <h2 className="text-2xl font-bold">Exercícios</h2>
+//             <label htmlFor="addExercise" className="btn btn-outline btn-primary">Adicionar Exercício</label>
+//           </div>
 
-      <EditExercise exercise={editingExercise.exercise} onSave={saveEditedExercise} type={selectedWorkoutTab} />
-    </Layout>
-  )
-}
+//           <div className="flex flex-wrap justify-between">
+//             {
+//               (exercises[selectedWorkoutTab] || []).map((exercise, index) => (
+//                 <CardExercise
+//                   key={index}
+//                   {...exercise}
+//                   onEditExercise={() => editExercise(exercise, selectedWorkoutTab, index)}
+//                   onDeleteExercise={() => deleteExercise(selectedWorkoutTab, index)}
+//                 />
+//               ))
+//             }
+//           </div>
+//         </div>
+
+//       </div>
+
+//       <AddExercise onAddExercise={addExercise} selectedWorkoutTab={selectedWorkoutTab} />
+
+//       <EditExercise exercise={editingExercise.exercise} onSave={saveEditedExercise} type={selectedWorkoutTab} />
+//     </Layout>
+//   )
+// }
