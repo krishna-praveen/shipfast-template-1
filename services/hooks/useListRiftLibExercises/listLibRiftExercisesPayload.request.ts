098d4dc8aad43efedaf7d611ee17d816a175fda0
jeffsLM <jeffes.dev@gmail.com>
2024-03-10 21:40:03 -0300

    feat: use hook rift exercises

--- services/hooks/useListRiftLibExercises/listLibRiftExercisesPayload.request.ts
+++ services/hooks/useListRiftLibExercises/listLibRiftExercisesPayload.request.ts
@@ -6,7 +6,7 @@ import { IListRiftLibExercisesResponse, IListRiftLibExercisesPayload } from '@/t
 export const listRiftLibExercisesPayloadRequest = async (payload: IListRiftLibExercisesPayload): Promise<IListRiftLibExercisesResponse> => {
   const session = await useSession()
 
-  let data = await apiServer.get("exercises", {
+  let data = await apiServer.get("exercises/search", {
     params: { ...payload },
     headers: {
       Authorization: `${session.access_token}`,
