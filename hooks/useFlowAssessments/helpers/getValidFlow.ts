22fde3b63430dc7962437190b261201278bf9fff
jeffsLM <jeffes.dev@gmail.com>
2024-04-15 00:22:30 -0300

    fix: context acess

--- hooks/useFlowAssessments/helpers/getValidFlow.ts
+++ hooks/useFlowAssessments/helpers/getValidFlow.ts
@@ -9,12 +9,12 @@ interface GetValidFlowProps {
 export const getValidFlow = ({ key, page, initialFlow }: GetValidFlowProps) => {
   const flow = FLOWS.filter(flow => flow.type === key)[0];
 
-  const currentIndexPage = flow.pages.findIndex(flowPage => flowPage === page);
-  const nextPage = currentIndexPage !== -1 && flow.pages[currentIndexPage + 1];
+  const currentIndexPage = flow?.pages?.findIndex(flowPage => flowPage === page);
+  const nextPage = currentIndexPage !== -1 && flow?.pages[currentIndexPage + 1];
 
   return {
     ...flow,
     hasNextPage: !!nextPage,
-    nextPage: initialFlow ? flow.pages[0] : nextPage,
+    nextPage: initialFlow ? flow?.pages[0] : nextPage,
   }
 }
